88735493623e197e2dfdbff91febf8ace377574f
Ling <lingliang@umail.ucsb.edu>
2018-11-16 16:04:38 -0800

    config and traceGen

--- tetrisPerfSim/configs.py
+++ tetrisPerfSim/configs.py
@@ -5,7 +5,7 @@ Created on Nov 14, 2018
 
 This file sweeps configuration setups to perform design space exploration
 '''
-
+import json
 from tetrisPerfSim import components, traceGen
 
 # @liu: design the experiment: list the hardware setup
@@ -32,7 +32,50 @@ def Benchmarking(select): # input: int/string, output: traceGen.App()
   # add code to fill in different app with different setups (sparse ratio, sourse etc) according to 'select'
   app = traceGen.App()
   # [TODO]:
+  model_info = {}
+  if select == 'vgg16':
+    with open('vgg.json', 'r') as f:
+      model_info = json.load(f)['vgg16']
+    app.name = select
+    app.numLayer = 16
+
   for i in range(0, app.numLayer):
     # [TODO]:
+    app.layers.append(traceGen.Layer())
+
+    #value
+    app.layers[i].value = {
+      'Batch': model_info['batch'],
+      'H': model_info['size_FM'][i][0], 'W': model_info['size_FM'][i][1],
+      'Cin': model_info['channel'][i][0], 'Cout': model_info['channel'][i][1],
+      'Kh': model_info['size_K'][i][0], 'Kw': model_info['size_K'][i][1]
+    }
+
+    # type
+    if i < 13:
+      app.layers[i].type = 'conv'
+    else:
+      app.layers[i].type = 'fc'
+
+    # sparseRatio
+    app.layers[i].sparseRatio = model_info['pruning_rates'][i]
+
+    # dataSource
+    app.layers[i].dataSource = 'synthatic'
+
+    # dataWidth
+    app.layers[i].dataWidth = 4
+
+    # name
+    if i < 13:
+      app.layers[i].name = 'conv' + str(i + 1)
+    else:
+      app.layers[i].name = 'fc' + str(i - 12)
+
+    # blockSizeH & blockSizeW
+    app.layers[i].blockSizeH = model_info['block_sizes'][i][0]
+    app.layers[i].blockSizeW = model_info['block_sizes'][i][1]
+
     traceGen.SparseDataGen(app.layers[i])
+
   return app 
