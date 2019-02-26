3ab9e22d404fb1f4042f46a83ad795895f2a80ea
Ling LIANG <39837540+liangling76@users.noreply.github.com>
2019-02-26 11:55:24 -0800

    Update reorderEngine.py

--- tetrisPerfSim/reorderEngine.py
+++ tetrisPerfSim/reorderEngine.py
@@ -16,8 +16,8 @@ import numpy as np
 # tranceGen.Layer()
 # Sechduler.SRAM()
 # output:
-# FMI_adr list[np3d]
-# FMO_adr list[np3d]
+# FMI_adr list[np1d for each block]
+# FMO_adr list[np1d for each block]
 def AdrGen(layer, sram):
 
   # assume the size of input feature map equals output feature map (stride = 1)
@@ -29,24 +29,35 @@ def AdrGen(layer, sram):
   Kh = layer.value['Kh']
   numBank = sram.numBank
   blockList = layer.data
+  P = 1 * 2  # padding
 
   if layer.type == 'conv':
-    FMI_order = np.reshape(np.arange(FMI_C * FM_H * FM_W), (FMI_C, FM_H, FM_W)) % numBank
-    FMO_order = np.reshape(np.arange(FMO_C * FM_H * FM_W), (FMO_C, FM_H, FM_W)) % numBank
+    FMI_order_org = np.reshape(np.arange(FMI_C * (FM_H + P) * (FM_W + P)), (FMI_C, (FM_H + P), (FM_W + P))) % numBank
+    FMI_order = np.zeros((FMI_C, FM_H * FM_W * Kw * Kh)).astype(int)
+    step = Kw * Kh
+    for i in range(FM_H):
+      for j in range(FM_W):
+        begin_idx = (i * FM_W + j) * step
+        FMI_order[:, begin_idx: (begin_idx + step)] = np.reshape(FMI_order_org[:, i: i + Kh, j: j + Kw], (FMI_C, step))
+
+    FMO_order = np.reshape(np.arange(FMO_C * FM_H * FM_W), (FMO_C, FM_H * FM_W)) % numBank
     W_order = np.reshape(np.arange(FMI_C * FMO_C * Kw * Kh), (FMI_C, FMO_C, Kw, Kh)) % numBank
+
   else:
     FMI_order = np.arange(FMI_C) % numBank
     FMO_order = np.arange(FMO_C) % numBank
     W_order = np.reshape(np.arange(FMI_C * FMO_C), (FMI_C, FMO_C)) % numBank
-
 
   FMI_adr = [] # adr for each block`s input FM
   FMO_adr = [] # adr for each block`s output FM
   W_adr = [] # adr for each block`s weight
 
   for block in blockList:
-    FMI_adr.append(FMI_order[block.indexRow])
-    FMO_adr.append(FMO_order[block.indexCol])
-    W_adr.append(W_order[block.indexRow, block.indexCol, :, :])
+    FMI_adr.append(np.reshape(FMI_order[block.indexRow], -1))
+    FMO_adr.append(np.reshape(FMO_order[block.indexCol], -1))
+    if layer.type == 'conv':
+      W_adr.append(W_order[block.indexRow, block.indexCol, :, :])
+    else:
+      W_adr.append(W_order[block.indexRow, block.indexCol])
 
   return FMI_adr, FMO_adr, W_adr
